7c8989c47597bd0528fdfca7fe87bf4efb853cf5
pyataevsv <pyataevsv@gmail.com>
2020-07-16 09:42:36 +0300

    Updates

---
+++ precache-manifest.2e72227266a82f098f69e1d03bbe58bb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc1ffa184ed17e1bb3c7f628a3de41eb",
+    "url": "/yandex-flow/index.html"
+  },
+  {
+    "revision": "ed0e69bbf70068035af4",
+    "url": "/yandex-flow/static/css/main.9b4096ff.chunk.css"
+  },
+  {
+    "revision": "8a6cb46e55830b0e455e",
+    "url": "/yandex-flow/static/js/2.d4f9bdd7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yandex-flow/static/js/2.d4f9bdd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed0e69bbf70068035af4",
+    "url": "/yandex-flow/static/js/main.fcb614ac.chunk.js"
+  },
+  {
+    "revision": "f0341e4fada4d5bf7cbe",
+    "url": "/yandex-flow/static/js/runtime-main.0691beb6.js"
+  },
+  {
+    "revision": "4d58ad22b9c922b55544b5cb579750b9",
+    "url": "/yandex-flow/static/media/chat-animation.4d58ad22.mp4"
+  },
+  {
+    "revision": "f8012af36a6e63def8f66d6e8517cd3b",
+    "url": "/yandex-flow/static/media/feature-theory.f8012af3.jpg"
+  },
+  {
+    "revision": "993a8662f3e5f0771cf3cc76567f44cc",
+    "url": "/yandex-flow/static/media/trainer-animation.993a8662.mp4"
+  }
+]);
